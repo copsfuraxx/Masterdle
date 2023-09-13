ab5a80a4695b4c16fb8021c2327d48aea727f41b
ThomVAT73200 <thomasnou@orange.fr>
2023-09-13 13:32:37 +0200

    :bug: fix entete de page

--- front/src/app/nav-bar/nav-bar.component.ts
+++ front/src/app/nav-bar/nav-bar.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit} from '@angular/core';
+import { Router} from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { Observable, of, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
   isConnected: boolean = false;
+  route$: Observable<string> = new Observable<string>();
 
-  constructor(private router: Router, private authService: AuthService) {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+  ) {
    this.isConnected = this.authService.isAuthenticatedUser();
    console.log(this.isConnected);
   }
 
+  ngOnInit(): void {
+    this.route$ = this.router.events.pipe(
+      switchMap(() =>
+      {
+        return of(this.router.url.split('/')[1]);
+      })
+    );
+  }
+
   handleClickLogIn() {
-    this.router.navigate(['/login']);
+    this.router.navigate(['login']);
   }
 }
